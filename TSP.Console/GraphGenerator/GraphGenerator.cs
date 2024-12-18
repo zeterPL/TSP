0dc60cee54ef2e17ea7054ef719476abcd7cbc5b
zeterPL <ukaszjanowicz@gmail.com>
2024-12-18 18:19:37 +0100

    GraphGenerator and TwoOpt Solver

---
+++ TSP.Console/GraphGenerator/GraphGenerator.cs
@@ -0,0 +1,31 @@
+﻿using TSP.Console.Files.Importer;
+
+namespace TSP.Console.GraphGenerator
+{
+    public static class GraphGenerator
+    {
+        /// <summary>
+        /// Generuje losową instancję TSP z podaną liczbą miast i zakresem współrzędnych.
+        /// </summary>
+        /// <param name="numberOfCities">Liczba miast.</param>
+        /// <param name="coordinateRange">Zakres współrzędnych (np. 100 oznacza współrzędne w przedziale [0,100]).</param>
+        /// <returns>Lista węzłów (miast) z ich współrzędnymi.</returns>
+        public static List<Node> GenerateRandomCities(int numberOfCities, int coordinateRange = 100)
+        {
+            Random random = new Random();
+            var cities = new List<Node>();
+
+            for (int i = 1; i <= numberOfCities; i++)
+            {
+                cities.Add(new Node
+                {
+                    Id = i,
+                    X = random.NextDouble() * coordinateRange,
+                    Y = random.NextDouble() * coordinateRange
+                });
+            }
+
+            return cities;
+        }
+    }
+}
---
+++ TSP.Console/Solver/TwoOptSolver.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP.Console.Solver
+{
+    public static class TwoOptSolver
+    {
+        /// <summary>
+        /// Rozwiązuje problem TSP przy użyciu heurystyki 2-opt.
+        /// </summary>
+        /// <param name="distanceMatrix">Macierz odległości między miastami.</param>
+        /// <returns>Najlepsza znaleziona trasa i jej długość.</returns>
+        public static Chromosome Solve(double[,] distanceMatrix)
+        {
+            int numberOfCities = distanceMatrix.GetLength(0);
+
+            int[] initialRoute = Enumerable.Range(0, numberOfCities).ToArray();
+            Random random = new Random();
+            initialRoute = initialRoute.OrderBy(x => random.Next()).ToArray();
+
+            return TwoOptImprove(initialRoute, distanceMatrix);
+        }
+
+        /// <summary>
+        /// Optymalizuje trasę przy użyciu heurystyki 2-opt.
+        /// </summary>
+        private static Chromosome TwoOptImprove(int[] route, double[,] distanceMatrix)
+        {
+            bool improvement = true;
+
+            while (improvement)
+            {
+                improvement = false;
+                for (int i = 0; i < route.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < route.Length; j++)
+                    {
+                        double oldDistance = CalcSegmentDistance(route, distanceMatrix, i, i + 1) +
+                                             CalcSegmentDistance(route, distanceMatrix, j, (j + 1) % route.Length);
+
+                        ReverseSegment(route, i + 1, j);
+
+                        double newDistance = CalcSegmentDistance(route, distanceMatrix, i, i + 1) +
+                                             CalcSegmentDistance(route, distanceMatrix, j, (j + 1) % route.Length);
+
+                        if (newDistance < oldDistance)
+                        {
+                            improvement = true;
+                        }
+                        else
+                        {
+                            ReverseSegment(route, i + 1, j); // Przywróć trasę
+                        }
+                    }
+                }
+            }
+            return new Chromosome(route, distanceMatrix);
+        }
+
+        /// <summary>
+        /// Odwraca segment trasy.
+        /// </summary>
+        private static void ReverseSegment(int[] route, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = route[start];
+                route[start] = route[end];
+                route[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        /// <summary>
+        /// Oblicza odległość między dwoma segmentami w trasie.
+        /// </summary>
+        private static double CalcSegmentDistance(int[] route, double[,] distanceMatrix, int from, int to)
+        {
+            return distanceMatrix[route[from], route[to]];
+        }
+    }
+}
